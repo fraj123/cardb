26ec5a3e92c2863bf7adddf7154e0190a176e2d8
Franz Mejia <mejiafrnz@gmail.com>
2022-09-29 20:55:48 -0400

    Feat: Complete Car domain

--- src/main/java/com/packt/cardb/domain/Car.java
+++ src/main/java/com/packt/cardb/domain/Car.java
@@ -1,7 +1,134 @@
 package com.packt.cardb.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String brand, model, color, registerName;
+    private int years, price;
+
+    public Car() {}
+
+    /**
+     * @param brand
+     * @param model
+     * @param color
+     * @param registerName
+     * @param years
+     * @param price
+     */
+    public Car(String brand, String model, String color, String registerName, int years, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerName = registerName;
+        this.years = years;
+        this.price = price;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the registerName
+     */
+    public String getRegisterName() {
+        return registerName;
+    }
+
+    /**
+     * @param registerName the registerName to set
+     */
+    public void setRegisterName(String registerName) {
+        this.registerName = registerName;
+    }
+
+    /**
+     * @return the years
+     */
+    public int getYears() {
+        return years;
+    }
+
+    /**
+     * @param years the years to set
+     */
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
 }
